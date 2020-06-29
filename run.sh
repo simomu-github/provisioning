c1d698facf0debea582f97359a9c61dac95b528d
simomu-github <simomutter@gmail.com>
2020-06-29 15:38:31 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if !(type ansible > /dev/null 2>&1); then
+	sudo apt update
+	sudo apt install ansible
+fi
+
+ansible-playbook ubuntu.yml --ask-become-pass $@
+
+if [ -d $HOME/dotfiles ]; then
+	bash $HOME/dotfiles/setup.sh
+fi
