a5ba1e1e6804b00065c382f920986d4357ec2fcc
simomu-github <simomutter@gmail.com>
2020-09-29 22:27:32 +0900

    add docker, docker-compose

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -6,5 +6,5 @@ EXECUTE_DIR=$(cd $(dirname $0) && pwd)
 
 export USER="test"
 
-${EXECUTE_DIR}/../run.sh --check
-${EXECUTE_DIR}/../run.sh
+${EXECUTE_DIR}/../run.sh --skip-tags=skip_test --check
+${EXECUTE_DIR}/../run.sh --skip-tags=skip_test
